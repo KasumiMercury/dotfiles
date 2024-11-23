1e0e6da8e816bdc24e75ef7f0f773f1c655031d2
KasumiMercury <naoki_shira@outlook.jp>
2024-11-23 21:20:48 +0900

    lazy load oil / conflict "nvim ."

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,6 +3,18 @@ return {
 	---@module 'oil'
 	---@type oil.setupopts
 	opts = {},
+
+	cmd = 'Oil',
+	keys = {
+		{
+			'<Bslash><Bslash>',
+			function()
+				require('oil').toggle_float()
+			end,
+			desc = 'Oil',
+		},
+	},
+
 	-- optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	dependencies = { "nvim-tree/nvim-web-devicons" },
