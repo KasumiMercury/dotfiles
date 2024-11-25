d8347f6ff033596c995ea6ae13000a38d965d8f4
KasumiMercury <naoki_shira@outlook.jp>
2024-11-25 11:18:51 +0900

    change lsp keybinds

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -62,22 +62,23 @@ vim.api.nvim_create_autocmd('lspattach', {
 		local set = vim.keymap.set
 		set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { buffer = true })
 		set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { buffer = true })
+		set('n', 'td', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { buffer = true })
+		set('n', 'gt', '<C-t>')
 		set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', { buffer = true })
 		set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', { buffer = true })
 		set('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', { buffer = true })
-		set('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', { buffer = true })
-		set('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', { buffer = true })
-		set('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
+		set('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', { buffer = true })
+		set('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', { buffer = true })
+		set('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
 			{ buffer = true })
-		set('n', '<space>d', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { buffer = true })
-		set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = true })
-		set('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', { buffer = true })
+		set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = true })
+		set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', { buffer = true })
 		set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', { buffer = true })
-		set('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', { buffer = true })
+		set('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', { buffer = true })
 		set('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', { buffer = true })
 		set('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', { buffer = true })
-		set('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', { buffer = true })
-		set('n', '<space>f', '<cmd>lua vim.lsp.buf.format()<cr>', { buffer = true })
+		set('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', { buffer = true })
+		set('n', '<Space>f', '<cmd>lua vim.lsp.buf.format()<cr>', { buffer = true })
 	end,
 })
 
