f13baffc46c798bb8df7afba7446d35c86a53704
KasumiMercury <naoki_shira@outlook.jp>
2025-05-29 16:21:11 +0900

    fix

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -105,7 +105,7 @@ vim.api.nvim_create_autocmd('lspattach', {
 require("quicker").setup()
 
 require('mason').setup()
-require('mason-lspconfig').setup()
+-- require('mason-lspconfig').setup()
 -- require('mason-lspconfig').setup_handlers {
 -- 	function(server_name)
 -- 		require('lspconfig')[server_name].setup {}
