3ad04a4cf57c71b30631a453ce5a436ab3dbbd07
KasumiMercury <naoki_shira@outlook.jp>
2026-02-17 09:52:58 +0900

    show hidden files as default in oil.nvim

--- nvim/lua/plugins/file-manager/oil.lua
+++ nvim/lua/plugins/file-manager/oil.lua
@@ -5,6 +5,16 @@ return {
 	opts = {
 		default_file_explorer = true,
 		user_default_keymaps = true,
+		view_options = {
+			show_hidden = true,
+			is_hidden_file = function(name, bufnr)
+				local m = name:match("^%.")
+				return m ~= nil
+			end,
+			is_always_hidden = function(name)
+				return false
+			end,
+		}
 	},
 
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
